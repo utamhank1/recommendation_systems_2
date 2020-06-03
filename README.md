cf1be8372fbd817949aaaa691c6f4d12c9902f0f
Ujjwal Tamhankar <utamhank1@gmail.com>
2020-06-02 22:32:22 -0400

    Added vectors_pt1.py, updated readme

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# utamhank1-recommendation_systems_2
+# recommendation_systems_2
  Repository to hold coursework from the LinkedIn Learning course: Machine Learning and AI Foundations: Recommendations
---
+++ chapter_3/vectors_pt1.py
@@ -0,0 +1,22 @@
+def main():
+    ratings = [5,
+               2,
+               3,
+               3,
+               4,
+               5,
+               5,
+               1,
+               5,
+               1,
+               3,
+               4
+               ]
+
+    for i, value in enumerate(ratings):
+        print(f"Updating rating: {i}")
+        ratings[i] = value*2
+        
+
+if __name__ == "__main__":
+    main()
